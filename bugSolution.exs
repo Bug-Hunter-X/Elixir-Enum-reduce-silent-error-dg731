7103bb5fe9717b30c415f4773267273528720738
BugHunter-X <askzenai@gmail.com>
2025-01-20 07:10:04 +0100

    Add bugSolution.exs file

---
+++ bugSolution.exs
@@ -0,0 +1,12 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+Enum.reduce(list, 0, fn x, acc ->
+  if rem(x, 2) == 0 do
+    acc + x
+  else
+    acc
+  end
+  acc  # Return the accumulator value
+end)
+```
